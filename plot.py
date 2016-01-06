213ed41e772c5e01963ca32059825c832e869ec9
Pavel Semashko <pavel.semashko@gmail.com>
2016-01-07 02:19:41 +0300

    Read from file only once

--- plot.py
+++ plot.py
@@ -2,8 +2,13 @@
 
 import matplotlib, matplotlib.pyplot as plt
 import argparse
+from collections import namedtuple
 from sys import argv
-from math import pi
+from math import pi, sqrt
+
+
+SimulationInfo = namedtuple('SimulationInfo', 'header, bots')
+BotInfo = namedtuple('BotInfo', 'header, data')
 
 def main():
     if len(argv) < 2 or len(argv) > 3:
@@ -15,48 +20,49 @@ def main():
     else:
         output_prefix = 'plot'
 
-    f = open(argv[1], "r")
-    num_bots = eval(f.readline())["num_bots"]
-    for i in xrange(num_bots):
-        d = eval(f.readline())
-        print "Plotting", str(d["id"]) + "..."
-        plot(argv[1], d["id"], output_prefix + '_' + d["id"] + ".png")
+    info = read_info(argv[1])
+    num_bots = info.header['num_bots']
+    for bot in info.bots.values():
+        print "Plotting", str(bot.header['id']) + "..."
+        plot(bot.header, bot.data, output_prefix + '_' + bot.header["id"] + ".png")
 
 
-def plot(filename, id, output_filename):
-
+def read_info(filename):
     f = open(filename, "r")
+    header = eval(f.readline())
+    bots = {}
+    num_bots = header['num_bots']
+    for i in xrange(num_bots):
+        bot_header = eval(f.readline())
+        bots[bot_header['id']] = BotInfo(header=bot_header, data = [])
+
+    for line in f:
+        d = eval(line)
+        bots[d['id']].data.append(d)
+
+    return SimulationInfo(header=header, bots=bots)
+
+
+def plot(header, data, output_filename):
 
     #plt.rcParams['legend.framealpha'] = 0.5
-
-    num_bots = eval(f.readline())["num_bots"]
-    title_data = None
-    for i in xrange(num_bots):
-        d = eval(f.readline())
-        if d["id"] == id:
-            title_data = d
-    if title_data == None:
-        raise RuntimeError, "id " + id + ": data not found in file header"
 
     time_data = []
     ex_data = []
     ey_data = []
     et_data = []
-    real_ex_data = []
-    real_ey_data = []
+    real_e_data = []
     real_et_data = []
     v_data = []
     omega_data = []
-    for line in f:
-        d = eval(line)
-        if d["id"] != id:
-            continue
+    for d in data:
         time_data.append(d["time"])
         ex_data.append(d["e_x"])
         ey_data.append(d["e_y"])
         et_data.append(d["e_theta"])
-        real_ex_data.append(d["real_e_x"])
-        real_ey_data.append(d["real_e_y"])
+        real_ex = d["real_e_x"]
+        real_ey = d["real_e_y"]
+        real_e_data.append(sqrt(real_ex**2 + real_ey**2))
         real_et_data.append(d["real_e_theta"])
         v_data.append(d["v"])
         omega_data.append(d["omega"])
@@ -71,30 +77,27 @@ def plot(filename, id, output_filename):
     #lgd = axes.legend(ncol=1, loc='center right', bbox_to_anchor=(-0.15, 0.5))
     lgd = axes.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
     legends.append(lgd)
-    axes.set_ylim([-1.0, 1.0])
 
     axes = plt.subplot(323)
-    real_ex, = axes.plot(time_data, real_ex_data, '-', label=r'real $e_x$')
-    real_ey, = axes.plot(time_data, real_ey_data, '-', label=r'real $e_y$')
+    real_e, = axes.plot(time_data, real_e_data, '-', label=r'$\Vert e_x\Vert$')
     axes.grid()
     #lgd = axes.legend(ncol=1, loc='center right', bbox_to_anchor=(-0.15, 0.5))
     lgd = axes.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
     legends.append(lgd)
-    axes.set_ylim([-1.0, 1.0])
 
     axes = plt.subplot(322)
     et, = axes.plot(time_data, et_data, 'r-', label=r'$e_\theta$')
     axes.grid()
     lgd = axes.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
     legends.append(lgd)
-    axes.set_ylim([-pi, pi])
+    #axes.set_ylim([-pi, pi])
 
     axes = plt.subplot(324)
     real_et, = axes.plot(time_data, real_et_data, 'r-', label=r'real $e_\theta$')
     axes.grid()
     lgd = axes.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
     legends.append(lgd)
-    axes.set_ylim([-pi, pi])
+    #axes.set_ylim([-pi, pi])
 
     axes = plt.subplot(325)
     ev, = axes.plot(time_data, v_data, 'g-', label=r'$v$')
